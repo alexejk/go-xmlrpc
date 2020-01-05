8c0c52b464bf976af3a83d47dfdfcf174b6ae630
Alexej Kubarev <alexej.kubarev@accedo.tv>
2020-01-05 01:11:11 +0100

    Basic decoding implementation added.
    Partially WIP as missing struct & array support as well as fault parsing

---
+++ decode.go
@@ -0,0 +1,152 @@
+package xmlrpc
+
+import (
+	"encoding/base64"
+	"encoding/xml"
+	"fmt"
+	"reflect"
+	"strconv"
+	"time"
+)
+
+type respWrapper struct {
+	Params []respParam `xml:"params>param"`
+	Fault  *respFault  `xml:"fault,omitempty"`
+}
+
+type respParam struct {
+	Value respValue `xml:"value"`
+}
+
+type respValue struct {
+	Array    []respValue        `xml:"array>data>value"`
+	Struct   []respStructMember `xml:"struct>member"`
+	String   string             `xml:"string"`
+	Int      string             `xml:"int"`
+	Int4     string             `xml:"i4"`
+	Double   string             `xml:"double"`
+	Boolean  string             `xml:"boolean"`
+	DateTime string             `xml:"dateTime.iso8601"`
+	Base64   string             `xml:"base64"`
+
+	Raw string `xml:",innerxml"` // the value can be default string
+}
+
+type respStructMember struct {
+	Name  string    `xml:"name"`
+	Value respValue `xml:"value"`
+}
+
+type respFault struct {
+	Value respValue `xml:"value"`
+}
+
+func (f respFault) IsEmpty() bool {
+	return len(f.Value.Struct) == 0
+}
+
+func DecodeResponse(body string, v interface{}) error {
+
+	wrapper := &respWrapper{}
+	if err := xml.Unmarshal([]byte(body), wrapper); err != nil {
+		return err
+	}
+
+	if wrapper.Fault != nil {
+		// TODO: Handle fault response
+	}
+
+	// Validate that v has same number of public fields as response params
+	if err := fieldsMustEqual(v, len(wrapper.Params)); err != nil {
+		return err
+	}
+
+	vElem := reflect.Indirect(reflect.ValueOf(v))
+	for i, param := range wrapper.Params {
+		field := vElem.Field(i)
+
+		if err := decodeValue(&param.Value, &field); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func decodeValue(value *respValue, field *reflect.Value) error {
+
+	var val interface{}
+	var err error
+
+	switch {
+
+	case value.Int != "":
+		val, err = strconv.Atoi(value.Int)
+
+	case value.Int4 != "":
+		val, err = strconv.Atoi(value.Int4)
+
+	case value.Double != "":
+		val, err = strconv.ParseFloat(value.Double, 64)
+
+	case value.Boolean != "":
+		val, err = decodeBoolean(value.Boolean)
+
+	case value.String != "":
+		val, err = value.String, nil
+
+	case value.Base64 != "":
+		val, err = decodeBase64(value.Base64)
+
+	case value.DateTime != "":
+		val, err = decodeDateTime(value.DateTime)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	if val != nil {
+		field.Set(reflect.ValueOf(val))
+	}
+
+	return nil
+}
+
+func decodeBoolean(value string) (bool, error) {
+
+	switch value {
+	case "1", "true", "TRUE", "True":
+		return true, nil
+	case "0", "false", "FALSE", "False":
+		return false, nil
+	}
+	return false, fmt.Errorf("unrecognized value '%s' for boolean", value)
+}
+
+func decodeBase64(value string) ([]byte, error) {
+
+	return base64.StdEncoding.DecodeString(value)
+}
+
+func decodeDateTime(value string) (time.Time, error) {
+
+	return time.Parse(time.RFC3339, value)
+}
+
+func fieldsMustEqual(v interface{}, expectation int) error {
+
+	vElem := reflect.Indirect(reflect.ValueOf(v))
+	numFields := 0
+	for i := 0; i < vElem.NumField(); i++ {
+		if vElem.Field(i).CanInterface() {
+			numFields++
+		}
+	}
+
+	if numFields != expectation {
+		return fmt.Errorf("number of exported fields (%d) on response type doesnt match expectation (%d)", numFields, expectation)
+	}
+
+	return nil
+}
---
+++ decode_test.go
@@ -0,0 +1,106 @@
+package xmlrpc
+
+import (
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDecodeResponse(t *testing.T) {
+	tests := []struct {
+		name     string
+		testFile string
+		v        interface{}
+		expect   interface{}
+		err      error
+	}{
+		{
+			name:     "simple response",
+			testFile: "response_simple.xml",
+			v: &struct {
+				Param string
+				Int   int
+			}{},
+			expect: &struct {
+				Param string
+				Int   int
+			}{
+				Param: "South Dakota",
+				Int:   12345,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			err := DecodeResponse(loadTestFile(t, tt.testFile), tt.v)
+			assert.Equal(t, tt.err, err)
+			assert.EqualValues(t, tt.expect, tt.v)
+		})
+	}
+}
+
+func Test_fieldsMustEqual(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  interface{}
+		expect int
+		err    error
+	}{
+		{
+			name:   "empty struct",
+			input:  struct{}{},
+			expect: 0,
+		},
+		{
+			name: "no exported fields",
+			input: struct {
+				priv int
+			}{
+				priv: 3,
+			},
+			expect: 0,
+		},
+		{
+			name: "exported fields",
+			input: struct {
+				Pub int
+			}{
+				Pub: 3,
+			},
+			expect: 1,
+		},
+		{
+			name: "mixed exported/unexported fields",
+			input: struct {
+				priv int
+				Pub  int
+			}{
+				Pub:  3,
+				priv: 4,
+			},
+			expect: 1,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			err := fieldsMustEqual(tt.input, tt.expect)
+			assert.Equal(t, tt.err, err)
+		})
+	}
+}
+
+func loadTestFile(t *testing.T, name string) string {
+
+	path := filepath.Join("testdata", name) // relative path
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return string(bytes)
+}
